7bbf9c03a08bf760802cf809ac54c2c8c74ef106
Laura Głąb <151255733+LauraGlab@users.noreply.github.com>
2024-09-09 20:43:20 +0200

    Coin.jsx

--- src/Component/Coin.jsx
+++ src/Component/Coin.jsx
@@ -1,40 +1,40 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import { SparkLineChart } from "@mui/x-charts";
 import "./Styles/Coin.css";
 
-function Coin (props
-){
+export default function Coin(props) {
+  const navigate = useNavigate();
+  const NavigatePage = () => {
+    navigate(`/CoinPage/${props.id}`);
+  };
 
-    const navigate = useNavigate();
-    const NavigatePage = () => {
-      navigate(`/CoinPage/${props.id}`);
-    };
-
-    return (
-      <div className="coin__container">
-        <div className="coin__row">
-          <div className="coinData">
-            <div className="coin">
-              <img src={props.icon} />
-              <h1 className="coinName">{props.coinName}</h1>
-              <p className="coinSymbol">{props.coinSymbol}</p>
-              <p className="coinPrice">$ {props.price.toFixed(2)}</p>
-              {props.priceChange < 0 ? (
-                <p className="priceChange red">{props.priceChange.toFixed(2)}%</p>
-              ) : (
-                <p className="priceChange green">{props.priceChange.toFixed(2)}%</p>
-              )};
-              <p className="coinVolume">$ {props.marketCap.toLocaleString()}</p>
-              <button
-                onClick={NavigatePage}
-              >
-                More Info
-              </button>
-            </div>
+  return (
+    <div className="coin__section">
+      <div className="coin">
+        <div className="firstSection">
+          <div className="coinImg__section">
+            <img src={props.icon} alt="Crypto icon" className="coinImg" />
+          </div>
+          <div className="coinTitle">
+            <h2 className="coinSymbol">{props.coinSymbol}</h2>
+            <p className="coinName">{props.coinName}</p>
+          </div>
+          <div>
+            <SparkLineChart data={props.sparkline} height={100} />
           </div>
         </div>
-      </div>
-    );
-}
 
-export default Coin;
+        <p className="coinPrice">$ {props.price.toFixed(2)}</p>
+        {props.priceChange < 0 ? (
+          <p className="priceChange red">- {props.priceChange.toFixed(2)}%</p>
+        ) : (
+          <p className="priceChange green">+ {props.priceChange.toFixed(2)}%</p>
+        )}
+        <p className="coinVolume">$ {props.marketCap.toLocaleString()}</p>
+        <button onClick={NavigatePage} className="coinBtn">
+          More Info
+        </button>
+      </div>
+    </div>
+  );
+}
