3b398861895e326cf3cf95ed421ca21f652c4937
Laura Głąb <151255733+LauraGlab@users.noreply.github.com>
2024-09-10 23:53:28 +0200

    Update Coin.jsx

--- src/Component/Coin.jsx
+++ src/Component/Coin.jsx
@@ -1,10 +1,11 @@
 import { useNavigate } from "react-router-dom";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 import "./Styles/Coin.css";
 
 export default function Coin(props) {
   const navigate = useNavigate();
   const NavigatePage = () => {
-    navigate(`/CoinPage/${props.id}`);
+    navigate(`/CoinPage/${props.id}?sparkline=true`);
   };
 
   return (
@@ -18,20 +19,32 @@ export default function Coin(props) {
             <h2 className="coinSymbol">{props.coinSymbol}</h2>
             <p className="coinName">{props.coinName}</p>
           </div>
-          <div>
-          </div>
         </div>
-
-        <p className="coinPrice">$ {props.price.toFixed(2)}</p>
-        {props.priceChange < 0 ? (
-          <p className="priceChange red">- {props.priceChange.toFixed(2)}%</p>
-        ) : (
-          <p className="priceChange green">+ {props.priceChange.toFixed(2)}%</p>
-        )}
-        <p className="coinVolume">$ {props.marketCap.toLocaleString()}</p>
-        <button onClick={NavigatePage} className="coinBtn">
-          More Info
-        </button>
+        <div className="secondSection">
+          <Sparklines
+            id="sparkline"
+            svgWidth={100}
+            svgHeight={50}
+            data={props.sparkline_7d.price}
+          >
+            <SparklinesLine color="hsl(240, 73%, 61%)" />
+          </Sparklines>
+        </div>
+        <div className="thirdSection">
+          <p className="coinPrice">${props.price.toFixed(2)}</p>
+          {props.priceChange < 0 ? (
+            <p className="priceChange red">{props.priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="priceChange green">
+              +{props.priceChange.toFixed(2)}%
+            </p>
+          )}
+        </div>
+        <div className="fourthSection">
+          <button onClick={NavigatePage} className="coinBtn">
+            More Info
+          </button>
+        </div>
       </div>
     </div>
   );
