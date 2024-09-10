bdf5953e6a502bb99c6029e8d82a59ce6388d190
Laura Głąb <151255733+LauraGlab@users.noreply.github.com>
2024-09-10 23:50:10 +0200

    Update CoinPage.jsx

--- src/Routes/CoinPage.jsx
+++ src/Routes/CoinPage.jsx
@@ -1,68 +1,89 @@
+import React from "react";
 import { useState, useEffect } from "react";
-import { useParams, Link} from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 import "./Styles/CoinPage.css";
 
-function CoinPage() {
-let { id } = useParams();
-const [coin, setCoin] = useState(null);
+export default function CoinPage() {
+  let { id } = useParams();
+  const [coin, setCoin] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-
+  
   useEffect(() => {
     const controller = new AbortController();
-    fetch(`https://api.coingecko.com/api/v3/coins/${id}`,{signal: controller.signal})
-    .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          setCoin(data);
-        });
-        return () => controller.abort();
-    }, []);
 
-    
-      if (coin !== null) {
-        return (
-          <div className="coinPage__container">
-            <div className="coinInfo__container">
-              <img src={coin.image.small} alt="coin image" className="coinPage__image"/>
-              <h1>{coin.name}</h1>
-              <div className="coinPage__row">
-                <h3 className="coinPage__rowHeader">Symbol:</h3>
-                <h3 className="coinPage__rowData">{coin.symbol}</h3>
-              </div>
-              <div className="coinPage__row">
-                <h3 className="coinPage__rowHeader">Current Price:</h3>
-                <h3 className="coinPage__rowData">
-                  $ {coin.market_data.market_cap.usd.toLocaleString()}
-                </h3>
-              </div>
-              <div className="coinPage__row">
-                <h3 className="coinPage__rowHeader">Total Volume:</h3>
-                <h3 className="coinPage__rowData">
-                  $ {coin.market_data.total_volume.usd.toLocaleString()}
-                </h3>
-              </div>
-              <div className="coinPage__row">
-                <h3 className="coinPage__rowHeader">24hr High:</h3>
-                <h3 className="coinPage__rowData green">
-                  $ {coin.market_data.high_24h.usd.toLocaleString()}
-                </h3>
-              </div>
-              <div className="coinPage__row">
-                <h3 className="coinPage__rowHeader">24hr Low:</h3>
-                <h3 className="coinPage__rowData red">
-                  $ {coin.market_data.low_24h.usd.toLocaleString()}
-                </h3>
-              </div>
+    setIsLoading(true);
+    fetch(`https://api.coingecko.com/api/v3/coins/${id}?sparkline=true`, {
+      signal: controller.signal,
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        setCoin(data);
+      });
+    return () => controller.abort();
+  }, []);
 
-              <Link to="/">
-                <div className="coinPage__button">Go back</div>
-              </Link>
+  if (coin !== null) {
+    return (
+      <div className="coinPage__section">
+        <div className="coinPage__firstSection">
+          <Link to="/">
+            <div className="coinPage__button">Go back</div>
+          </Link>
+          <div>
+            <img
+              src={coin.image.small}
+              alt="coin image"
+              className="coinPage__image"
+            />
+            <h1>{coin.name}</h1>
+          </div>
+        </div>
+        <div className="coinPage__secondSection">
+          <div>
+            <Sparklines
+              id="sparkline"
+              svgWidth={100}
+              svgHeight={50}
+              data={coin.sparkline_7d}
+            >
+              <SparklinesLine color="hsl(240, 73%, 61%)" />
+            </Sparklines>
+          </div>
+          <div>
+            <div className="coinPage__row">
+              <h3 className="coinPage__rowHeader">Current Price:</h3>
+              <h3 className="coinPage__rowData">
+                $ {coin.market_data.market_cap.usd.toLocaleString()}
+              </h3>
+            </div>
+            <div className="coinPage__row">
+              <h3 className="coinPage__rowHeader">Total Volume:</h3>
+              <h3 className="coinPage__rowData">
+                $ {coin.market_data.total_volume.usd.toLocaleString()}
+              </h3>
+            </div>
+            <div className="coinPage__row">
+              <h3 className="coinPage__rowHeader">24hr High:</h3>
+              <h3 className="coinPage__rowData green">
+                $ {coin.market_data.high_24h.usd.toLocaleString()}
+              </h3>
+            </div>
+            <div className="coinPage__row">
+              <h3 className="coinPage__rowHeader">24hr Low:</h3>
+              <h3 className="coinPage__rowData red">
+                $ {coin.market_data.low_24h.usd.toLocaleString()}
+              </h3>
             </div>
           </div>
-        );
-      } else {
-        return null; 
-      }
-  } 
-export default CoinPage;
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
