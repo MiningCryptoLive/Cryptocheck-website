ade8e510b254bbe3e41dec8d2820c65e4339b1a8
Laura Głąb <151255733+LauraGlab@users.noreply.github.com>
2024-09-10 23:49:17 +0200

    Update CoinsStats.jsx

--- src/Routes/CoinsStats.jsx
+++ src/Routes/CoinsStats.jsx
@@ -3,10 +3,31 @@ import Coin from "./../Component/Coin.jsx";
 import Footer from "./../Component/Footer.jsx";
 import "./Styles/CoinsStats.css";
 
-export default function CoinsStats() {
+export default function CoinsStats(props) {
   const [coins, setCoins] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true",
+      {
+        signal: controller.signal,
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        setCoins(data);
+      });
+
+    return () => controller.abort();
+  }, []);
 
   const searchCoin = (e) => {
     setSearchTerm(e.target.value);
@@ -16,42 +37,23 @@ export default function CoinsStats() {
     coin.name.toUpperCase().includes(searchTerm.toUpperCase())
   );
 
-    useEffect(() => {
-      const controller = new AbortController();
-
-      setIsLoading(true);
-      fetch(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true", {
-          signal: controller.signal
-        })
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-
-          setIsLoading(false);
-          setCoins(data);
-        });
-
-        return () => controller.abort();
-    }, []);
-
-
   return (
-    <div className="coinsStatsSection">
-      <input
-        type="text"
-        placeholder="Search the coin..."
-        onChange={searchCoin}
-        className="inputSearch"
-      ></input>
+    <div className="coinsStats___section">
+      <div className="inputSearch__section">
+        <input
+          type="text"
+          placeholder="Search the coin..."
+          onChange={searchCoin}
+          className="inputSearch"
+        />
+      </div>
       <div className="crypto__section">
-        {isLoading && <h1 className="loadingMssg">Data Loading...</h1>}
+        {isLoading && <h1 className="loader">Loading...</h1>}
         {filterCoins.map((coins) => {
           return (
             <>
               <Coin
-                key={coins.id}
+                key={coins.name}
                 id={coins.id}
                 icon={coins.image}
                 coinName={coins.name}
@@ -60,14 +62,12 @@ export default function CoinsStats() {
                 marketCap={coins.market_cap}
                 priceChange={coins.price_change_percentage_24h}
                 sparkline={coins.sparkline_in_7d.price}
+                isLoading={isLoading}
               />
             </>
           );
         })}
       </div>
-      <Footer/>
     </div>
   );
 }
-
-
